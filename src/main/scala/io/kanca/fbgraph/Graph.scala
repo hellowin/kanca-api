cdf7e4b2f2f0a9a82683f610031c61fde58fc98a
Andi N. Dirgantara <andi.n.dirgantara@gmail.com>
2017-12-18 23:58:22 +0700

    add intellij code style

--- src/main/scala/io/kanca/fbgraph/Graph.scala
+++ src/main/scala/io/kanca/fbgraph/Graph.scala
@@ -1,55 +1,64 @@
 package io.kanca.fbgraph
 
-import scalaj.http._
 import play.api.libs.json._
+
+import scalaj.http._
 
 case class From(name: String, id: String)
 
 case class MessageTag(
-                       id: String,
-                       name: String,
-                       typ: String,
-                       offset: Int,
-                       length: Int,
-                     )
+  id: String,
+  name: String,
+  typ: String,
+  offset: Int,
+  length: Int,
+)
 
 case class GroupFeed(
-                      id: String,
-                      caption: Option[String],
-                      createdTime: String,
-                      description: Option[String],
-                      from: From,
-                      link: Option[String],
-                      message: Option[String],
-                      messageTags: List[MessageTag],
-                      name: Option[String],
-                      permalinkUrl: String,
-                      picture: Option[String],
-                      statusType: Option[String],
-                      story: Option[String],
-                      typ: String,
-                      updatedTime: String,
-                    )
+  id: String,
+  caption: Option[String],
+  createdTime: String,
+  description: Option[String],
+  from: From,
+  link: Option[String],
+  message: Option[String],
+  messageTags: List[MessageTag],
+  name: Option[String],
+  permalinkUrl: String,
+  picture: Option[String],
+  statusType: Option[String],
+  story: Option[String],
+  typ: String,
+  updatedTime: String,
+)
+
+case class FBListResult(data: List[JsObject], next: Option[String])
 
 object Graph {
 
-  val FB_URL = sys.env("FB_URL")
-  val DEFAULT_PAGE_LIMIT: Int = sys.env("DEFAULT_PAGE_LIMIT").toInt
-  val DEFAULT_REQUEST_LIMIT = sys.env("DEFAULT_REQUEST_LIMIT")
+  private val FB_URL = sys.env("FB_URL")
+  private val DEFAULT_PAGE_LIMIT: Int = sys.env("DEFAULT_PAGE_LIMIT").toInt
+  private val DEFAULT_REQUEST_LIMIT = sys.env("DEFAULT_REQUEST_LIMIT")
+
+  private def getListResult(req: HttpRequest): FBListResult = {
+    val resString: String = req.asString.body
+    val rawJson: JsValue = Json.parse(resString)
+    val data: List[JsObject] = (rawJson \ "data").validate[JsArray].getOrElse(Json.arr()).as[List[JsObject]]
+    val next: Option[String] = (rawJson \ "paging" \ "next").validate[String].asOpt
+
+    FBListResult(data, next)
+  }
 
   def getGroupFeeds(token: String, groupId: String, pageLimit: Int = DEFAULT_PAGE_LIMIT, nextUrl: String = null, results: List[GroupFeed] = List()): List[GroupFeed] = {
-    val resString: HttpResponse[String] = Http(s"$FB_URL/$groupId/feed")
+    val FBListResult(data, next) = getListResult(Http(s"$FB_URL/$groupId/feed")
       .params(Seq(
+        "method" -> "GET",
         "fields" -> "created_time,id,message,updated_time,caption,story,description,from,link,name,picture,status_type,type,shares,permalink_url,to,message_tags",
         "limit" -> DEFAULT_REQUEST_LIMIT,
         "access_token" -> token
-      ))
-      .asString
+      )).postForm)
 
-    val rawJson: JsValue = Json.parse(resString.body)
-    val data: JsArray = (rawJson \ "data").validate[JsArray].getOrElse(Json.arr())
-
-    val newResults: List[GroupFeed] = results ::: data.validate[JsArray].getOrElse(Json.arr()).as[List[JsObject]]
+    val newResults: List[GroupFeed] = results ::: data
       .map { rawFeed =>
         GroupFeed(
           (rawFeed \ "id").validate[String].get,
@@ -81,11 +90,9 @@ object Graph {
         )
       }
 
-    val next: String = (rawJson \ "paging" \ "next").validate[String].getOrElse(null)
+    if (next.orNull == null || pageLimit - 1 <= 0) return newResults
 
-    if (next == null || pageLimit - 1 <= 0) return newResults
-
-    getGroupFeeds(token, groupId, pageLimit - 1, next, newResults)
+    getGroupFeeds(token, groupId, pageLimit - 1, next.get, newResults)
   }
 
 }
