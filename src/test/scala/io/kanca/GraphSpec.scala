e7e3fec467711704d3fa92938441fe1998b4c8ec
Andi N. Dirgantara <andi.n.dirgantara@gmail.com>
2017-12-19 00:42:12 +0700

    fix wrong spec

--- src/test/scala/io/kanca/GraphSpec.scala
+++ src/test/scala/io/kanca/GraphSpec.scala
@@ -12,16 +12,16 @@ class GraphSpec extends FlatSpec with Matchers {
 
   s"Group feeds fetcher with page limit $DEFAULT_PAGE_LIMIT and request limit $DEFAULT_REQUEST_LIMIT" should "able to get feeds by default arguments" in {
     val feeds: List[GroupFeed] = Graph.getGroupFeeds(USER_TOKEN, GROUP_ID)
-    feeds.map(_.id).toSeq.size should be <= DEFAULT_PAGE_LIMIT * DEFAULT_REQUEST_LIMIT
-    feeds.map(_.id).toSeq.size should be > 1 * DEFAULT_REQUEST_LIMIT
-    feeds.map(_.id).toSeq.size shouldEqual feeds.size
+    feeds.map(_.id).toSet.size should be <= DEFAULT_PAGE_LIMIT * DEFAULT_REQUEST_LIMIT
+    feeds.map(_.id).toSet.size should be > 1 * DEFAULT_REQUEST_LIMIT
+    feeds.map(_.id).toSet.size shouldEqual feeds.size
     feeds.map{ feed => feed.isInstanceOf[GroupFeed] shouldEqual true}
   }
 
   it should "able to get feeds by page limit arguments" in {
     val feeds: List[GroupFeed] = Graph.getGroupFeeds(USER_TOKEN, GROUP_ID, 2)
-    feeds.map(_.id).toSeq should have size 2 * DEFAULT_REQUEST_LIMIT
-    feeds.map(_.id).toSeq.size shouldEqual feeds.size
+    feeds.map(_.id).toSet should have size 2 * DEFAULT_REQUEST_LIMIT
+    feeds.map(_.id).toSet.size shouldEqual feeds.size
     feeds.map{ feed => feed.isInstanceOf[GroupFeed] shouldEqual true}
   }
 
