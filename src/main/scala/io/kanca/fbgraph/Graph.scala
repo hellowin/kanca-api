5639be8c5b6d875234a4fc6f06053e5644354c34
Andi N. Dirgantara <andi.n.dirgantara@gmail.com>
2017-12-18 23:34:27 +0700

    implement dotenv and fix test

--- src/main/scala/io/kanca/fbgraph/Graph.scala
+++ src/main/scala/io/kanca/fbgraph/Graph.scala
@@ -3,70 +3,83 @@ package io.kanca.fbgraph
 import scalaj.http._
 import play.api.libs.json._
 
-case class From (name: String, id: String)
+case class From(name: String, id: String)
 
-case class MessageTag (
-  id: String,
-  name: String,
-  typ: String,
-  offset: Int,
-  length: Int,
-)
+case class MessageTag(
+                       id: String,
+                       name: String,
+                       typ: String,
+                       offset: Int,
+                       length: Int,
+                     )
 
-case class GroupFeed (
-  id: String,
-  caption: Option[String],
-  createdTime: String,
-  description: Option[String],
-  from: From,
-  link: Option[String],
-  message: Option[String],
-  messageTags: List[MessageTag],
-  name: Option[String],
-  permalinkUrl: String,
-  picture: Option[String],
-  statusType: Option[String],
-  story: Option[String],
-  typ: String,
-  updatedTime: String,
-)
+case class GroupFeed(
+                      id: String,
+                      caption: Option[String],
+                      createdTime: String,
+                      description: Option[String],
+                      from: From,
+                      link: Option[String],
+                      message: Option[String],
+                      messageTags: List[MessageTag],
+                      name: Option[String],
+                      permalinkUrl: String,
+                      picture: Option[String],
+                      statusType: Option[String],
+                      story: Option[String],
+                      typ: String,
+                      updatedTime: String,
+                    )
 
 object Graph {
 
-  def getGroupFeeds(token: String, groupId: String, pageLimit: Int = 10, nextUrl: String = null, results: List[GroupFeed] = List()): List[GroupFeed] = {
-    val resString: HttpResponse[String] = Http(s"https://graph.facebook.com/v2.11/$groupId/feed?fields=created_time,id,message,updated_time,caption,story,description,from,link,name,picture,status_type,type,shares,permalink_url,to,message_tags&limit=100&access_token=$token")
+  val FB_URL = sys.env("FB_URL")
+  val DEFAULT_PAGE_LIMIT: Int = sys.env("DEFAULT_PAGE_LIMIT").toInt
+  val DEFAULT_REQUEST_LIMIT = sys.env("DEFAULT_REQUEST_LIMIT")
+
+  def getGroupFeeds(token: String, groupId: String, pageLimit: Int = DEFAULT_PAGE_LIMIT, nextUrl: String = null, results: List[GroupFeed] = List()): List[GroupFeed] = {
+    val resString: HttpResponse[String] = Http(s"$FB_URL/$groupId/feed")
+      .params(Seq(
+        "fields" -> "created_time,id,message,updated_time,caption,story,description,from,link,name,picture,status_type,type,shares,permalink_url,to,message_tags",
+        "limit" -> DEFAULT_REQUEST_LIMIT,
+        "access_token" -> token
+      ))
       .asString
 
     val rawJson: JsValue = Json.parse(resString.body)
     val data: JsArray = (rawJson \ "data").validate[JsArray].getOrElse(Json.arr())
 
     val newResults: List[GroupFeed] = results ::: data.validate[JsArray].getOrElse(Json.arr()).as[List[JsObject]]
-      .map{ rawFeed => GroupFeed(
-        (rawFeed \ "id").validate[String].get,
-        (rawFeed \ "caption").validate[String].asOpt,
-        (rawFeed \ "created_time").validate[String].get,
-        (rawFeed \ "description").validate[String].asOpt,
-        From(
-          (rawFeed \ "from" \ "name").validate[String].get,
-          (rawFeed \ "from" \ "id").validate[String].get
-        ),
-        (rawFeed \ "link").validate[String].asOpt,
-        (rawFeed \ "message").validate[String].asOpt,
-        (rawFeed \ "message_tags").validate[List[JsObject]].getOrElse(List()).map { obj => MessageTag(
-          (obj \ "id").validate[String].get,
-          (obj \ "name").validate[String].get,
-          (obj \ "type").validate[String].get,
-          (obj \ "offset").validate[Int].get,
-          (obj \ "length").validate[Int].get
-        ) },
-        (rawFeed \ "name").validate[String].asOpt,
-        (rawFeed \ "permalink_url").validate[String].get,
-        (rawFeed \ "picture").validate[String].asOpt,
-        (rawFeed \ "status_type").validate[String].asOpt,
-        (rawFeed \ "story").validate[String].asOpt,
-        (rawFeed \ "type").validate[String].get,
-        (rawFeed \ "updated_time").validate[String].get
-      ) }
+      .map { rawFeed =>
+        GroupFeed(
+          (rawFeed \ "id").validate[String].get,
+          (rawFeed \ "caption").validate[String].asOpt,
+          (rawFeed \ "created_time").validate[String].get,
+          (rawFeed \ "description").validate[String].asOpt,
+          From(
+            (rawFeed \ "from" \ "name").validate[String].get,
+            (rawFeed \ "from" \ "id").validate[String].get
+          ),
+          (rawFeed \ "link").validate[String].asOpt,
+          (rawFeed \ "message").validate[String].asOpt,
+          (rawFeed \ "message_tags").validate[List[JsObject]].getOrElse(List()).map { obj =>
+            MessageTag(
+              (obj \ "id").validate[String].get,
+              (obj \ "name").validate[String].get,
+              (obj \ "type").validate[String].get,
+              (obj \ "offset").validate[Int].get,
+              (obj \ "length").validate[Int].get
+            )
+          },
+          (rawFeed \ "name").validate[String].asOpt,
+          (rawFeed \ "permalink_url").validate[String].get,
+          (rawFeed \ "picture").validate[String].asOpt,
+          (rawFeed \ "status_type").validate[String].asOpt,
+          (rawFeed \ "story").validate[String].asOpt,
+          (rawFeed \ "type").validate[String].get,
+          (rawFeed \ "updated_time").validate[String].get
+        )
+      }
 
     val next: String = (rawJson \ "paging" \ "next").validate[String].getOrElse(null)
 
--- src/test/scala/io/kanca/GraphSpec.scala
+++ src/test/scala/io/kanca/GraphSpec.scala
@@ -5,18 +5,20 @@ import io.kanca.fbgraph.{Graph, GroupFeed}
 
 class GraphSpec extends FlatSpec with Matchers {
 
-  val userToken = "EAAFhcjTi6hIBAHb3shaLzFWRZAcQadDnfIkJHGCXsZBVfZCfZCUOhD7IXMkg7ZAkrLoWOY7m5H8ha3ZBLhgr4KVDZAEZAaQlDnf64NIgnVjGtISYfCFLg5hYITsCVpAYsEKZBzcSNJDL0rK7mVG70wRQnhKiLQZCywJFc99Q7pp0Ky0cqJ4HbEdGRIzUz3MQmCoxuxIhnM4IVusAZDZD"
-  val groupId = "1920036621597031"
+  val USER_TOKEN = sys.env("USER_TOKEN")
+  val GROUP_ID = sys.env("GROUP_ID")
+  val DEFAULT_PAGE_LIMIT: Int = sys.env("DEFAULT_PAGE_LIMIT").toInt
+  val DEFAULT_REQUEST_LIMIT: Int = sys.env("DEFAULT_REQUEST_LIMIT").toInt
 
   "Group fetcher" should "able to get feeds by default arguments" in {
-    val feeds: List[GroupFeed] = Graph.getGroupFeeds(userToken, groupId)
-    feeds.size should be <= 10*100
-    feeds.size should be > 100
+    val feeds: List[GroupFeed] = Graph.getGroupFeeds(USER_TOKEN, GROUP_ID)
+    feeds.size should be <= DEFAULT_PAGE_LIMIT * DEFAULT_REQUEST_LIMIT
+    feeds.size should be > 1 * DEFAULT_REQUEST_LIMIT
   }
 
   it should "able to get feeds by page limit arguments" in {
-    val feeds: List[GroupFeed] = Graph.getGroupFeeds(userToken, groupId, 2)
-    feeds should have size 2*100
+    val feeds: List[GroupFeed] = Graph.getGroupFeeds(USER_TOKEN, GROUP_ID, 2)
+    feeds should have size 2 * DEFAULT_REQUEST_LIMIT
   }
 
 }
