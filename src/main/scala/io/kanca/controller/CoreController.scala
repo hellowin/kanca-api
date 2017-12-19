c0ced141fc553e3a3f03f91e2141d42cc4de349c
Andi N. Dirgantara <andi.n.dirgantara@gmail.com>
2017-12-19 10:48:40 +0700

    implement hello world controller for get group feeds

--- src/main/scala/io/kanca/controller/CoreController.scala
+++ src/main/scala/io/kanca/controller/CoreController.scala
@@ -2,11 +2,18 @@ package io.kanca.controller
 
 import com.twitter.finagle.http.Request
 import com.twitter.finatra.http.Controller
+import io.kanca.fbgraph.Graph
+
+case class GroupRequest(groupId: String, token: String)
 
 class CoreController extends Controller {
 
   get("/") { request: Request =>
     s"Server is ready"
+  }
+
+  get("/group-feeds") { req: Request =>
+    Graph.getGroupFeeds(req.getParam("token"), req.getParam("group_id"))
   }
 
 }
