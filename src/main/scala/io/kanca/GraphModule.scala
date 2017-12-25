3a3984efe263608f323b94f2f8e0678f59327a11
Andi N. Dirgantara <andi.n.dirgantara@gmail.com>
2017-12-25 12:54:24 +0700

    use provides instead of bind on module

--- src/main/scala/io/kanca/GraphModule.scala
+++ src/main/scala/io/kanca/GraphModule.scala
@@ -1,14 +1,13 @@
 package io.kanca
 
-import com.twitter.inject.{InjectorModule, TwitterModule}
+import com.google.inject.{Provides, Singleton}
+import com.twitter.inject.TwitterModule
 import io.kanca.fbgraph.FacebookGraph
 
 object GraphModule extends TwitterModule {
 
-  override val modules = Seq(InjectorModule)
-
-  override def configure() {
-    bindSingleton[Graph].to[FacebookGraph]
-  }
+  @Singleton
+  @Provides
+  def providesGraph: Graph = new FacebookGraph
 
 }
