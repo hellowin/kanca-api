76d5404f342f492355f6fc39354d55fa496db566
Andi N. Dirgantara <andi.n.dirgantara@gmail.com>
2017-12-28 21:31:28 +0700

    little fix

--- src/main/scala/io/kanca/controller/CoreController.scala
+++ src/main/scala/io/kanca/controller/CoreController.scala
@@ -2,9 +2,6 @@ package io.kanca.controller
 
 import com.twitter.finagle.http.Request
 import com.twitter.finatra.http.Controller
-import io.kanca.repository.{GroupFeedMySQL, RepositoryMySQL}
-
-case class GroupRequest(groupId: String, token: String)
 
 class CoreController extends Controller {
 
